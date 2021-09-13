47080e3ce8370de358b9872a5ff2b2e24eb0356d
Paulo Salvatore <salvatoregames@gmail.com>
2021-09-13 17:32:12 -0300

    feat(auth): implement login at service

---
+++ src/auth/UsuarioPayload.ts
@@ -0,0 +1,5 @@
+export interface UsuarioPayload {
+  username: string;
+
+  sub: number;
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,17 +5,28 @@ import { UsuarioService } from 'src/usuario/usuario.service';
 // Bcrypt
 import * as bcrypt from 'bcrypt';
 import { UnauthorizedError } from 'src/errors/unauthorized.error';
+import { JwtService } from '@nestjs/jwt';
+import { UsuarioToken } from './UsuarioToken';
+import { UsuarioPayload } from './UsuarioPayload';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usuarioService: UsuarioService) {}
+  constructor(
+    private readonly usuarioService: UsuarioService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async login(email: string, senha: string) {
+  async login(email: string, senha: string): Promise<UsuarioToken> {
     const usuario: Usuario = await this.validateUser(email, senha);
 
-    // Validar o usuário
-    // TODO: Montar o payload
-    // TODO: Transformar o payload em um JWT
+    const payload: UsuarioPayload = {
+      username: usuario.email,
+      sub: usuario.id,
+    };
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
   }
 
   private async validateUser(email: string, senha: string) {
