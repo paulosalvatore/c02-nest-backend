6fcf50b5bd47d18bb70f092c0f718955c8b297a8
Paulo Salvatore <salvatoregames@gmail.com>
2021-09-13 17:43:21 -0300

    feat(error): implement unauthorized error interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { UnauthorizedInterceptor } from './interceptors/unauthorized.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
+  // Pipes
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,6 +14,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // Interceptors
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
 
   await app.listen(3001);
 }
