1d9019df536c113ce5d91c01014b724a7383d9bd
Paulo Salvatore <salvatoregames@gmail.com>
2021-09-13 15:19:26 -0300

    feat(usuario): create CRUD files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonagemModule } from './personagem/personagem.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [PersonagemModule, PrismaModule],
+  imports: [PersonagemModule, PrismaModule, UsuarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
