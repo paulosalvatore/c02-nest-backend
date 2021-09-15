51e1f5710ee040b273dbec3f509b2859b484dee4
Paulo Salvatore <salvatoregames@gmail.com>
2021-09-15 15:26:23 -0300

    fix(auth): endpoint login should be public

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import { Public } from './public.decorator';
 export class AuthController {
   constructor(private readonly service: AuthService) {}
 
+  @Public()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   login(@Body() { email, senha }: LoginRequestBody) {
